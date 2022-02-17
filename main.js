edbdc20482481232af2e3e157f89f7b59fc14243
JJSEONG <94487454+JJSEONG@users.noreply.github.com>
2022-02-17 17:30:43 +0900

    regexp

---
+++ main.js
@@ -0,0 +1,9 @@
+let str = `
+010-1234-5678
+the@gmail.com
+http://www.omdbapi.com/?apikey=7035c60c&s=frozen
+The quick brown fox jumps over the lazy dog.
+abbcccddd
+`
+
+console.log(str.match(/\.$/gim))
